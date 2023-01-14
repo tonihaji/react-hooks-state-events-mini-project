528a1d22ddf3f54cd7e5fcb3d2df107288199139
ehachi <ehajianto@gmail.com>
2023-01-14 11:06:03 +0300

    Completed assignment

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
-function Task() {
+function Task({text,category}) {
+  const [deleted, setDeleted]=useState(false)
+  function handleDelete(){
+    setDeleted(true);
+  }
+  if (deleted){
+    return null;
+  }
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList(prop) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+       {prop.tasks.map((task) => (
+         <Task
+          key={task.text} 
+          text={task.text} 
+          category={task.category}
+          onDelete={prop.onDelete}
+          id={task.id}
+          
+          />
+        ))}
     </div>
   );
 }
-
 export default TaskList;
